5fb3aa7d8b4ae60930e53c53d387531a582412d0
John Bi <bxb100@outlook.com>
2019-12-03 00:47:08 +0800

    Initial commit

---
+++ ingest_tmdb_to_appserarch.py
@@ -0,0 +1,37 @@
+import json
+
+import requests
+
+
+def extract():
+    f = open('./tmdb.json')
+    if f:
+        return json.loads(f.read())
+    return {}
+
+
+def index_all(movieDict=None):
+    if movieDict is None:
+        movieDict = {}
+    for id, movie in movieDict.items():
+        index_doc(movie)
+        print(id)
+
+
+def index_doc(doc):
+    content = []
+    content.append(doc)
+    resp = requests.post("http://localhost:3002/api/as/v1/engines/tmdb/documents",
+                         headers={"content-type": "application/json",
+                                  "Authorization": "Bearer private-vmnsw8mvzebbcpfeje5rzhxg"},
+                         data=json.dumps(content))
+    print(resp)
+
+
+def main():
+    movieDict = extract()
+    index_all(movieDict=movieDict)
+
+
+if __name__ == "__main__":
+    main()
